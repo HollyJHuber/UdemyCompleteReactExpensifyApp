5ec4e6d631cea4a34819f4534699e18ec1539a11
HollyJHuber <hollyjhuber@gmail.com>
2018-08-15 15:41:57 -0400

    fetches data from firebase, adds summary

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
+import { startSetExpenses } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import { sortByAmount } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
@@ -13,12 +13,15 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-console.log("app.js is running");
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+})
+
