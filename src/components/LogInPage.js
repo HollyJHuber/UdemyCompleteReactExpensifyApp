1b225d269e0849ef0c744f8137a0e9ebacd9fe25
HollyJHuber <hollyjhuber@gmail.com>
2018-08-17 13:16:51 -0400

    adds login logout authorization

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -1,37 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
 
-export class LogInPage extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      loggedIn: false
-    };
-  }
-  onSubmit = (e) => {
-    e.preventDefault();
-    console.log('log in button clicked');
-    this.setState(() => { loggedIn: true});
-    this.props.history.push('/dashboard');
-  };
-  render() {
-    return (
-      <div>
-        <h1>Log In to Expensify</h1>
-        <h5>~ enter your username and password to see your expenses ~</h5>
-        <form onSubmit={this.onSubmit}>
-          <p>
-            <input type="text" placeholder="username" autoFocus></input>
-          </p>
-          <p>
-            <input type="password" placeholder="password"></input>
-          </p>
-          <p>
-            <button>LOG IN</button>
-          </p>
-        </form>
-      </div>
-    );
-  };
-};
+const LogInPage = ( { startLogin }) => (
+  <div>
+    <h3><button onClick={startLogin}>LOG IN</button></h3>
+  </div>
+);
 
-export default LogInPage;
+const mapDispatchToProps  = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LogInPage);
+
+// export class LogInPage extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       loggedIn: false
+//     };
+//   }
+//   onSubmit = (e) => {
+//     e.preventDefault();
+//     console.log('log in button clicked');
+//     this.setState(() => { loggedIn: true});
+//     this.props.history.push('/dashboard');
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h1>Log In to Expensify</h1>
+//         <h5>~ enter your username and password to see your expenses ~</h5>
+//         <form onSubmit={this.onSubmit}>
+//           <p>
+//             <input type="text" placeholder="username" autoFocus></input>
+//           </p>
+//           <p>
+//             <input type="password" placeholder="password"></input>
+//           </p>
+//           <p>
+//             <button>LOG IN</button>
+//           </p>
+//         </form>
+//       </div>
+//     );
+//   };
+// };
+
+// export default LogInPage;
