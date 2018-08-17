aeb094997d313869d69b9ca2ec71797c5ee5fabd
HollyJHuber <hollyjhuber@gmail.com>
2018-08-17 15:12:55 -0400

    adds PrivateRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
 import LogInPage from '../components/LogInPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboard';
 import AddExpensePage from '../components/AddExpense';
 import EditExpensePage from '../components/EditExpense';
 import HelpPage from '../components/Help';
 import NotFoundPage from '../components/NotFound';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history = {history}> 
   <div>
-    <Header />
     <Switch>
       <Route path="/" component={LogInPage} exact={true} />
-      <Route path="/dashboard" component={ExpenseDashboardPage} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit/:id" component={EditExpensePage} />
+      <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+      <PrivateRoute path="/create" component={AddExpensePage} />
+      <PrivateRoute path="/edit/:id" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
       <Route component={NotFoundPage}/>
     </Switch>
