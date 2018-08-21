991b7b5e01ff0b9d36ba574dece65f07a9dcd8f5
HollyJHuber <hollyjhuber@gmail.com>
2018-08-21 12:06:57 -0400

    adds login and header styles

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 const LogInPage = ( { startLogin }) => (
-  <div>
-    <h3><button onClick={startLogin}>LOG IN</button></h3>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify</h1>
+      <p>it's time to get your expenses under control</p>
+      <button className="button" onClick={startLogin}>Login with Google</button>
+    </div>
   </div>
 );
 
